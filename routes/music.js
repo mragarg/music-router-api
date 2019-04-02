3fd2bd45bb87fd9862fc703f97179ea00107cb4a
Ashish Garg <mragarg@gmail.com>
2019-04-02 14:16:00 -0400

    Adds handler and skeleton to the router file

---
+++ routes/music.js
@@ -0,0 +1,17 @@
+// Express
+const express = require('express');
+
+// Router
+const Router = express.Router;
+
+// Create the Router
+const musicRoutes = Router();
+
+// Handlers for Router
+// GET (retrieve)
+// POST (create)
+// PUT (update)
+// DELETE (delete)
+
+// Export router
+module.exports = musicRoutes;
